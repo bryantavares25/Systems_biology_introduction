9217fa04a74baab678e9acb9263afa647f386d1f
Bryan Tavares <bryantavares25@gmail.com>
2023-06-05 10:27:39 -0300

    Update tentamos.R

--- tentamos.R
+++ tentamos.R
@@ -7,16 +7,27 @@ library(AnnotationHub)
 # ?makeOrgPackageFromNCBI
 #makeOrgPackageFromNCBI(version = "0.1", author = "BART <bryantavares25@gmail.com>", maintainer = "BART <bryantavares25@gmail.com>", outputDir = ".", tax_id = "2100", genus = "Mesomycoplasma", species = "hyorhinis",)
 
-hub = AnnotationHub()
-query(hub, c("Escherichia coli"))
-
-org.EcK12.eg.db.sqlite
+#hub = AnnotationHub()
+#query(hub, c("Escherichia coli"))
+#org.EcK12.eg.db
 
 ##### rrvgo
 
-library(rrvgo)
-go_analysis <- read.delim(system.file("05_Network_centrality_modularity.txt", package="rrvgo"))
-simMatrix <- calculateSimMatrix(go_analysis$ID,
-                                orgdb="org.EcK12.eg.db.sqlite",
-                                ont="BP",
-                                method="Rel")
+#library(rrvgo)
+#go_analysis <- read.delim(system.file("05_Network_centrality_modularity.txt", package="rrvgo"))
+#simMatrix <- calculateSimMatrix(go_analysis$ID, orgdb="org.EcK12.eg.db", ont="BP", method="Rel")
+
+##### GOSemSim
+#install
+# if (!requireNamespace("BiocManager", quietly = TRUE))
+#install.packages("BiocManager")
+#BiocManager::install("GOSemSim")
+
+
+##### clusterProfiler
+
+#install
+# if (!requireNamespace("BiocManager", quietly = TRUE))
+#install.packages("BiocManager")
+#BiocManager::install("clusterProfiler")
+
