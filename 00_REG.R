0682a20990ea234604120237f0b413f9127b6a72
Bryan Tavares <106768423+bryantavares25@users.noreply.github.com>
2024-04-12 17:36:20 -0300

    Update

--- 00_REG.R
+++ 00_REG.R
@@ -7,7 +7,9 @@ library(readr)
 # Data import
 node_deg_bet <- read.csv("/home/regenera/Documents/BART_gitHub/Course_systems_biology_introduction/00_REG_node_bd.csv", stringsAsFactors = FALSE)
 
-
+# Chart and average lines for quadrant definition
+plot(node_deg_bet$Degree.unDir, node_deg_bet$Betweenness.unDir, xlab="Degree", ylab ="Betweenness", col = "black")
+abline(h = median(node_deg_bet$Betweenness.unDir), v = median(node_deg_bet$Degree.unDir))
 
 #
 
